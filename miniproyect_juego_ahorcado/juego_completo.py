e1c2798662e8cf9da89efdaeded3bfa70a03b862
AdrianMadrid <adrian_6996@hotmail.com>
2020-10-25 13:48:34 +0100

    inicio

---
+++ miniproyect_juego_ahorcado/juego_completo.py
@@ -0,0 +1,4 @@
+print("\n\n") #Hago dos saltos de linea para que no me quede tan arriba el texto
+print("                     ******-------------------¡JUEGO DEL AHORCADO!---------------------******") 
+#espaceo el titulo para que me quede más bonito
+
